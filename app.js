902cca9a7f49225c97b9762880410bf9e504b25a
Victor Ray <victor.ray22@gmail.com>
2020-05-23 00:04:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,24 @@ app.use(bodyParser.json());
 app.use("/graduates", gradRoutes);
 app.use("/", offerRoutes);
 
-// catch 404 and forward to error handler
-if (app.get("env") === "development") {
-  app.use((req, res, next) => {
-    var err = new Error("Not Found");
-    err.status = 404;
-    return next(err);
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+// catch 404 and forward to error handler
+
+app.use((req, res, next) => {
+  var err = new Error("Not Found");
+  err.status = 404;
+  return next(err);
+});
 
 // development error handler
 // will print stacktrace
@@ -30,17 +40,6 @@ app.use((err, req, res, next) => {
     error: err,
   });
 });
-
-/*if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
-  app.use(express.static("client/build"));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}*/
 
 //postgres://rliylvrkildwxr:9d5269b5a5b6346cc0f69c9a5f992097dc9233d5a4dff7d600473d0d8d3a55f7@ec2-54-165-36-134.compute-1.amazonaws.com:5432/det7cf4ii66ifv
 app.listen(process.env.PORT || 3000, () => {
