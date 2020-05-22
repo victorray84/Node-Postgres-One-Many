54ea8ad21989f4855927c65e1d0555f9125d322b
Victor Ray <victor.ray22@gmail.com>
2020-05-22 22:52:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,18 @@ app.use((err, req, res, next) => {
     error: err,
   });
 });
+
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //postgres://rliylvrkildwxr:9d5269b5a5b6346cc0f69c9a5f992097dc9233d5a4dff7d600473d0d8d3a55f7@ec2-54-165-36-134.compute-1.amazonaws.com:5432/det7cf4ii66ifv
 app.listen(process.env.PORT || 3000, () => {
   console.log("Getting started on port 8000!");
